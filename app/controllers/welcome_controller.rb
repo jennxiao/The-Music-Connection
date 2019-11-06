49c3f2be6d31a940507a5cbd9775c30f6645adbc
Jie Luo <jieluo@berkeley.edu>
2019-11-06 09:23:15 -0800

    Remove now-useless debug messages, oops

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,5 @@ class WelcomeController < ApplicationController
   def index
     a = AdminSettings.last
     session[:form_open] = a.form_open
-    puts "AAAAAAAAAAAAAAAAAAAAAA"
-    puts a.form_open
   end
 end
