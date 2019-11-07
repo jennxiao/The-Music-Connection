23447b292781b527ba88d07c97f77f8547f2ec6e
Jie Luo <jieluo@berkeley.edu>
2019-11-06 16:31:44 -0800

    Fix app crashing upon initialization when AdminSettings database entry does not exist

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
   def index
     a = AdminSettings.last
+    if a.nil?
+      session[:form_open] = false
+      return
+    end
     session[:form_open] = a.form_open
   end
 end
