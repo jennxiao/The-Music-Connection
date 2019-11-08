d26c4befbf336d5ba60c3534bb74e7394c342c43
Jie Luo <jieluo@berkeley.edu>
2019-11-07 22:43:41 -0800

    Delete old code

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -30,58 +30,17 @@ function init() {
   phoneNumberForm.addEventListener("keypress", addHyphens)
 
   function addHyphens() {
-      var numberValue = phoneNumberForm.value;
-      if (numberValue.length == 3) {
-        phoneNumberForm.value += "-";
-      } else if (numberValue.length == 7) {
-        phoneNumberForm.value += "-";
-      }
+    var numberValue = phoneNumberForm.value;
+    if (numberValue.length == 3) {
+      phoneNumberForm.value += "-";
+    } else if (numberValue.length == 7) {
+      phoneNumberForm.value += "-";
+    }
   }
-
-  //Event Listener for adding/removing instruments
-  // document.getElementById("rem_instr").style.display = "none";
-  // var instrument = document.getElementsByClassName("instrument")[0];
-  // if (instrument != null) {
-  //   instrument.addEventListener('change', function(){
-  //     display_other(instrument);
-  //   });
-  // }
-  // var add_instr = document.getElementById("add_instr");
-  // add_instr.addEventListener('click', function() {
-  //   var original = document.getElementsByClassName("instrument")[0];
-  //   var cln = original.cloneNode(true);
-
-  //   if (cln != null) {
-  //     cln.addEventListener('change', function(){
-  //       display_other(cln);
-  //     });
-  //   }
-  //   var other = cln.getElementsByClassName("instr_other")[0];
-  //   var val = cln.getElementsByTagName("select")[0].value;
-  //   if (val == "Others") {
-  //     other.style.display = 'block';
-  //   } else {
-  //     other.style.display = 'none';
-  //   }
-  //   document.getElementById("instruments").appendChild(cln);
-  //   if (document.getElementsByClassName("instrument").length > 1) {
-  //     document.getElementById("rem_instr").style.display = "inline-block";
-  //   }
-  // });
-  // var rem_instr = document.getElementById("rem_instr");
-  // rem_instr.addEventListener('click', function () {
-  //   var len = document.getElementsByClassName("instrument").length;
-  //   var elem = document.getElementsByClassName("instrument")[len - 1];
-  //   document.getElementById("instruments").removeChild(elem);
-  //   if (document.getElementsByClassName("instrument").length <= 1) {
-  //     document.getElementById("rem_instr").style.display = "none";
-  //   }
-  // });
 
   //Event Listener for adding/removing time availability
   var add_time = document.getElementById("add_time");
   add_time.addEventListener('click', function() {
-    console.log("logged!")
     var original = document.getElementsByClassName("time-group")[0];
     var cln = original.cloneNode(true);
     document.getElementById("time-groups").appendChild(cln);
