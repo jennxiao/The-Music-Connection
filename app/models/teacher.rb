18ebf628171ed6162acbce9382457a4947f4a3be
Jennifer Xiao <jennifer.xiao@stratim.com>
2019-11-24 19:09:13 -0800

    keep teacher model the same

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,7 +1,13 @@
 class Teacher < ActiveRecord::Base
   include ContactValidation
-  
-  has_many :classes
+  validates :class_name,          exclusion:  { in: [nil] }
+  validates :school_name,         exclusion:  { in: [nil] }
+  validates :grade,               presence: true,
+                                  numericality: { only_integer: true }
+  validates :class_time,      presence: true
+  validates :instrument,          presence: true
+  validates :instrument,             exclusion:  { in: [nil] }
+  #has_many :classes
   accepts_nested_attributes_for :classes, :reject_if => :all_blank, :allow_destroy => true
 
 end
