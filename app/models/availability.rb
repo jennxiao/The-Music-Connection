0346ac810a495b09c131e88ddea8bc37ac6d00bf
Jie Luo <jieluo@berkeley.edu>
2019-12-01 19:34:59 -0800

    Add unit tests for availability, fix serialization/deserialization

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -1,17 +1,21 @@
 # frozen_string_literal: true
 
+# Class representing a period of time determined by
+# day of the week, start, and end time
 class Availability
   attr_reader :weekday, :start_time, :end_time
 
   def initialize(weekday, start_time, end_time)
     @weekday = weekday
-    @start_time = Time.parse('Thu Jan 1 ' + start_time + ' 1970')
-    @end_time = Time.parse('Thu Jan 1 ' + end_time + ' 1970')
+    @start_time = Time.parse('Thu Jan 1 ' + start_time + ' 1970 -0000')
+    @end_time = Time.parse('Thu Jan 1 ' + end_time + ' 1970 -0000')
   end
 
   class << self
     def serialize(a)
-      a.weekday + '&' + a.start_time.to_s + '&' + a.end_time.to_s + ';'
+      # rubocop:disable LineLength
+      a.weekday + '&' + a.start_time.strftime('%H:%M') + ':00&' + a.end_time.strftime('%H:%M') + ':00;'
+      # rubocop:enable LineLength
     end
 
     def deserialize(a)
@@ -24,16 +28,17 @@ class Availability
       ret
     end
 
+    # Calculate overlap of times in seconds. Returns an integer
     def overlap(a, b)
       a, b = b, a if b.start_time < a.start_time
       # a starts and ends before b begins
       return 0 if a.end_time < b.start_time
       # a starts before b starts
       # a ends after b ends
-      return b.end_time - b.start_time if a.end_time > b.end_time
+      return (b.end_time - b.start_time).round if a.end_time > b.end_time
 
       # regular case overlap
-      a.end_time - b.start_time
+      (a.end_time - b.start_time).round
     end
   end
 end
