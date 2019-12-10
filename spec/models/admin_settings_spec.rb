843245b6882aeb4857d8f8a2f368ae75059c92a9
Jie Luo <jieluo@berkeley.edu>
2019-12-10 12:37:04 -0800

    Add AdminSettings rspec tests

--- spec/models/admin_settings_spec.rb
+++ spec/models/admin_settings_spec.rb
@@ -2,11 +2,34 @@
 
 require 'rails_helper'
 
+
+
 describe AdminSettings do
+  let(:setting) {
+    AdminSettings.new({
+      form_open: false,
+      salt: 'salt',
+      password_hash: BCrypt::Password.create('password'),
+      last_updated: Time.at(1),
+      email: 'placeholder@tmc.com',
+      session_id: 'placeholder'
+    })
+  }
   context 'Verify validations' do
-    it 'should only allow boolean values for form_open' do
-      get :welcome
-      expect(response).to redirect_to('/admin')
+    it 'successfully validates a valid AdminSettings' do
+      expect(setting.valid?).to be true
+    end
+    it 'fails missing password_hash' do
+      expect(setting.update_attributes(password_hash: nil)).to be false
+    end
+    it 'fails missing last_updated' do
+      expect(setting.update_attributes(last_updated: nil)).to be false
+    end
+    it 'fails invalid email' do
+      expect(setting.update_attributes(email: 'arbitrary string')).to be false
+    end
+    it 'fails missing session_id' do
+      expect(setting.update_attributes(session_id: nil)).to be false
     end
   end
 end
